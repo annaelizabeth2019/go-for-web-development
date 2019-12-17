3e1a39ef3cba9abee71ec6109c2f352ca5b517d7
annaelizabeth2019 <anna@justuno.com>
2019-12-17 11:46:06 -0600

    minor changes and restructuring

--- TCP/main.go
+++ TCP/main.go
@@ -34,9 +34,6 @@ func handle(conn net.Conn) {
 
 	// read (will read the connection, we want to return the URL)
 	request(conn)
-
-	// write (will return the URL)
-	respond(conn)
 }
 
 func request(conn net.Conn) {
@@ -47,9 +44,9 @@ func request(conn net.Conn) {
 		if i == 0 {
 			m := strings.Fields(ln)
 			fmt.Println("URL:::", m[1])
+			// so this is a weird router because normally it would look at the URL and divide by route THEN by method, but whatever, this is practice
 			if m[0] == "GET" {
-				handleGet(conn, m)
-
+				handleGet(conn, m[1])
 			}
 		}
 		if ln == "" {
@@ -59,17 +56,20 @@ func request(conn net.Conn) {
 	}
 }
 
-func handleGet(conn net.Conn, m []string) {
-	switch m[1] {
+// handles get requests
+func handleGet(conn net.Conn, m string) {
+	switch m {
 	case "/abc":
 		abc(conn)
 	case "/anna":
 		anna(conn)
 	default:
-		// some error page
+		// write (will return the URL -- first assignment)
+		respond(conn)
 	}
 }
 
+// Some random functions to handle different requests
 func abc(conn net.Conn) {
 	body := `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title></title></head><body><strong>You reached ABC</strong></body></html>`
 	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
@@ -79,7 +79,7 @@ func abc(conn net.Conn) {
 	fmt.Fprint(conn, body)
 }
 func anna(conn net.Conn) {
-	body := `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title></title></head><body><strong>You're a queen!</strong></body></html>`
+	body := `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title></title></head><body><h1>You're a queen!</h1></body></html>`
 	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
 	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
 	fmt.Fprint(conn, "Content-Type: text/html\r\n")
